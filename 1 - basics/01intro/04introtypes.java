326b12f56a9eb11c15d4fc157e2dc4741e253305
ganeshkrishnamurthybhat <gk.ganeshb@gmail.com>
2022-08-17 05:21:03 +0000

    Adding minor files for variables and types

--- 1 - basics/01intro/04introtypes.java
+++ 1 - basics/01intro/04introtypes.java
@@ -4,9 +4,11 @@ public class Introtypes {
      */
     public static void main(String[] args) {
         int variable = 1;
+        String stringvariable = "String variable testing";
         System.out.println("Testing variables");
         System.out.println(variable);
         System.out.println(String.valueOf(variable));
         System.out.println("Testing variables" + String.valueOf(variable));
+        System.out.println(stringvariable);
     }
 }
---
+++ 1 - basics/02variables/05introvariables.java
@@ -0,0 +1,7 @@
+public class Introvariables {
+
+    public static void main(String[] args) {
+
+    }
+
+}
