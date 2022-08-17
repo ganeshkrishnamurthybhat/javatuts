e8b16981e82175d7c2f69dd020543e975a6061bf
ganeshkrishnamurthybhat <gk.ganeshb@gmail.com>
2022-08-17 04:45:27 +0000

    Adingfiles for basics and types

---
+++ 1 - basics/01intro/03introtypes.java
@@ -0,0 +1,10 @@
+public class Introtypes {
+    /**
+     * 
+     */
+    public static void main(String[] args) {
+        int variable = 1;
+        System.out.println("Testing variables");
+        System.out.println(variable);
+    }
+}
---
+++ 1 - basics/01intro/04introtypes.java
@@ -0,0 +1,12 @@
+public class Introtypes {
+    /**
+     * 
+     */
+    public static void main(String[] args) {
+        int variable = 1;
+        System.out.println("Testing variables");
+        System.out.println(variable);
+        System.out.println(String.valueOf(variable));
+        System.out.println("Testing variables" + String.valueOf(variable));
+    }
+}
